42d5f34660efffa52d04b14fc2ccde7e6a06ec55
Sandip Mane <sandip2490@gmail.com>
2024-01-06 00:23:47 +0530

    Added component to display cart items and total cost

--- src/components/Checkout/constants.js
+++ src/components/Checkout/constants.js
@@ -40,3 +40,5 @@ export const CHECKOUT_FORM_VALIDATION_SCHEMA = yup.object().shape({
   city: yup.string().required(t("validations.cityRequired")),
   zipCode: yup.number().required(t("validations.zipCodeRequired")),
 });
+
+export const CHECKOUT_LOCAL_STORAGE_KEY = "checkoutUserDetails";
