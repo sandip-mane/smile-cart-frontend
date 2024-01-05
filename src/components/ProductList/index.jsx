af24043b206033e6aa404d03194269d43a4fa2ae
Sandip Mane <sandip2490@gmail.com>
2024-01-05 18:27:25 +0530

    Added pagination to product listing page

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,29 +1,36 @@
 import { useState } from "react";
 
 import { Header, PageLoader } from "components/commons";
-import { useShowProducts } from "hooks/reactQuery/useProductsApi";
+import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Pagination } from "neetoui";
 import { isEmpty } from "ramda";
 import { useTranslation } from "react-i18next";
 import withTitle from "utils/withTitle";
 
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const { t } = useTranslation();
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
   const [searchTerm, setSearchTerm] = useState("");
   const debouncedSearchTerm = useDebounce(searchTerm);
 
-  const { data: { products = [] } = {}, isLoading } = useShowProducts({
+  const queryParams = {
     searchTerm: debouncedSearchTerm,
-  });
+    page: currentPage,
+    pageSize: DEFAULT_PAGE_SIZE,
+  };
+
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts(queryParams);
 
   if (isLoading) return <PageLoader />;
 
   return (
-    <div className="px-6 pb-6">
+    <div className="flex flex-col px-6 pb-6">
       <Header
         title={t("title")}
         actionBlock={
@@ -32,7 +39,10 @@ const ProductList = () => {
             prefix={<Search />}
             type="search"
             value={searchTerm}
-            onChange={event => setSearchTerm(event.target.value)}
+            onChange={event => {
+              setCurrentPage(DEFAULT_PAGE_INDEX);
+              setSearchTerm(event.target.value);
+            }}
           />
         }
       />
@@ -46,6 +56,14 @@ const ProductList = () => {
             ))}
           </div>
         )}
+      </div>
+      <div className="mb-5 self-end">
+        <Pagination
+          count={totalProductsCount}
+          navigate={page => setCurrentPage(page)}
+          pageNo={currentPage}
+          pageSize={DEFAULT_PAGE_SIZE}
+        />
       </div>
     </div>
   );
--- src/hooks/reactQuery/useProductsApi.js
+++ src/hooks/reactQuery/useProductsApi.js
@@ -3,10 +3,11 @@ import { QUERY_KEYS } from "constants/query";
 import productsApi from "apis/products";
 import { useQuery } from "react-query";
 
-export const useShowProducts = params =>
+export const useFetchProducts = params =>
   useQuery({
     queryKey: [QUERY_KEYS.PRODUCTS, params],
     queryFn: () => productsApi.fetch(params),
+    keepPreviousData: true,
   });
 
 export const useShowProduct = slug =>
